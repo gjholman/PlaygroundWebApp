037aea0cd2509d33bf5db9a7fcd3b280685f3a8d
Garrett <holmangarrettj@gmail.com>
2022-02-26 13:42:53 -0700

    vite tsx tailwind headlessui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900 hover:text-4xl animate-bounce">
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
